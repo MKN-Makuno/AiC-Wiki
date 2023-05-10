7ae912cdfc41eb7f1e4bd82d2c143c8d184b2338
派兹 <admin@blw.moe>
2023-05-11 02:01:09 +0800

    Revert "refactor(docsify): refactoring Wiki site"

--- assets/js/contribution-button.js
+++
@@ -1,7 +0,0 @@
-function switchContributionPage() {
-    if(location.pathname == '/contribution_v2/') {
-        location.assign(`/`);
-    } else {
-        location.assign(`../../contribution_v2`);
-    }
-}
